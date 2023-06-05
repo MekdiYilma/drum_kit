6b4e4478da4cd68ae0cf731b82ad3f74f315c9ff
Mekdi Yilma <mekdiyilma@asana.com>
2023-06-05 11:13:23 -0700

    refactor makeSound in js file

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 var allButtons = document.querySelectorAll(".drum");
 
 for (i = 0; i < allButtons.length; i += 1) {
-  allButtons[i].addEventListener("click", handleClick);
+  allButtons[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+  });
 }
 
-function handleClick() {
-  var audio = new Audio("./sounds/tom-1.mp3");
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
+});
+
+var soundDict = {
+  w: "crash",
+  a: "kick",
+  s: "snare",
+  d: "tom-1",
+  j: "tom-2",
+  k: "tom-3",
+  l: "tom-4",
+};
+
+function makeSound(key) {
+  var sound = soundDict[key];
+  var audio = new Audio("./sounds/" + sound + ".mp3");
   audio.play();
 }
