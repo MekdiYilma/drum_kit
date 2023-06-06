0484ec7908bb356a72ca191fa817ec36f45d6212
Mekdi Yilma <mekdiyilma@asana.com>
2023-06-06 09:21:14 -0700

    add animation to buttons

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ for (i = 0; i < allButtons.length; i += 1) {
   allButtons[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML)
   });
 }
 
 document.addEventListener("keypress", function (event) {
   makeSound(event.key);
+  buttonAnimation(event.key)
 });
 
 var soundDict = {
@@ -25,4 +27,12 @@ function makeSound(key) {
   var sound = soundDict[key];
   var audio = new Audio("./sounds/" + sound + ".mp3");
   audio.play();
+}
+
+function buttonAnimation(key) {
+  document.querySelector("." + key).classList.add("pressed")
+  setTimeout(function() {
+    document.querySelector("." + key).classList.remove("pressed")  
+  }, 400);
+  
 }
